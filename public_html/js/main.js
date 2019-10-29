49a81f5ac7b69a869915c6449e733bd76fc5e20a
Zorica <zorica.mijatovic@cubes.rs>
2019-10-29 14:52:43 +0100

    finish online-section and clients-section

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // lead slider
     if ($('.lead-slider').length > 0) {
@@ -8,10 +8,83 @@ $(document).ready(function(){
             loop: true,
             slideBy: 1,
             autoplay: true,
-            autoplayTimeout: 2000,
+            autoplayTimeout: 3000,
             autoplayHoverPause: true
         });
     }
+
+    // clients slider
+    if ($('.clients-slider').length > 0) {
+        $('.clients-slider').owlCarousel({
+            items: 8,
+            dots: false,
+            loop: true,
+            slideBy: 1,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            nav: true,
+            navText: [
+                '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+                '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+            ],
+            navContainer: '.clients-links .custom-nav',
+            margin: 8,
+            responsive: {
+                0: {
+                    items: 2,
+                    margin: 4,
+                    nav: false
+                },
+                550: {
+                    items: 3,
+                    margin: 4,
+                    nav: true
+                },
+                700: {
+                    items: 6,
+                    margin: 6,
+                    nav:true
+                },
+                1000: {
+                    items: 8,
+                    slideBy: 1,
+                    margin: 8
+                }
+            }
+        });
+    }
+
+    // email validate
+
+    if ($('.contact-form')) {
+        $('.contact-form').validate({
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                email: {
+                    email: true
+                }
+            },
+            messages: {
+                email: {
+                    email: 'Molimo Vas unesite ispravnu email adresu.'
+                }
+            },
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest(".input-group").find(".error-msg"));
+            }
+
+        });
+    }
+
+
+
     //ANIMATION
     function animation() {
         let windowHeight = $(window).height();
